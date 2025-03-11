7099f367f165d47054fa5a5e75edd8587ae1fa22
vitalino silva <vitalino.silva@hicx.com>
2025-03-11 10:54:48 +0000

    fixed the MultiInstanceLoopCharacteristics

--- src/bpmn-workflow-editor/activiti-model-definitions/activiti-model-types/multi-instance-loop-characteristics.js
+++ src/bpmn-workflow-editor/activiti-model-definitions/activiti-model-types/multi-instance-loop-characteristics.js
@@ -1,33 +1,21 @@
-export default {
-    name: 'MultiInstanceLoopCharacteristics',
-    superClass: ['bpmn:MultiInstanceLoopCharacteristics'],
-    properties: [
-      { 
-        name: 'collection', 
-        type: 'String', 
-        isAttr: true, 
-        ns: { prefix: 'activiti', localName: 'collection', name: 'http://activiti.org/bpmn' }
-      },
-      { 
-        name: 'elementVariable', 
-        type: 'String', 
-        isAttr: true, 
-        ns: { prefix: 'activiti', localName: 'elementVariable', name: 'http://activiti.org/bpmn' }
-      },
-      { 
-        name: 'isSequential', 
-        type: 'Boolean', 
-        isAttr: true 
-      },
-      { 
-        name: 'loopCardinality', 
-        type: 'String', 
-        isAttr: true 
-      },
-      { 
-        name: 'completionCondition', 
-        type: 'String', 
-        isAttr: true 
-      }
-    ]
+export default{
+  name: 'MultiInstanceLoopCharacteristics',
+  superClass: ['bpmn:MultiInstanceLoopCharacteristics'],
+  properties: [
+    { 
+      name: 'collection', 
+      isAttr: true,
+      type: 'String',
+      ns: { prefix: 'activiti', localName: 'collection', name: 'http://activiti.org/bpmn' }
+    },
+    { 
+      name: 'elementVariable', 
+      isAttr: true,
+      type: 'String',
+      ns: { prefix: 'activiti', localName: 'elementVariable', name: 'http://activiti.org/bpmn' }
+    },
+    { name: 'isSequential', type: 'Boolean', isAttr: true },
+    { name: 'loopCardinality', type: 'String' },
+    { name: 'completionCondition', type: 'String' }
+  ]
 };
