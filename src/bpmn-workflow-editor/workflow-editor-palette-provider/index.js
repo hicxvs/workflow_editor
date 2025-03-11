6b682d176d61cacc091c69213bf0bb39e8ee7f4a
vitalino silva <vitalino.silva@hicx.com>
2025-03-11 10:50:43 +0000

    updated

--- src/bpmn-workflow-editor/workflow-editor-palette-provider/index.js
+++ src/bpmn-workflow-editor/workflow-editor-palette-provider/index.js
@@ -9,12 +9,8 @@ function WorkflowEditorPaletteProvider(palette, create, elementFactory, spaceToo
     );
   
     this.getPaletteEntries = function() {
-      // Retrieve and modify the palette entries from the parent class
       const entries = PaletteProvider.prototype.getPaletteEntries.call(this);
-  
-      // Remove specific entries
       ['create.data-store', 'create.data-object'].forEach(entry => delete entries[entry]);
-    
       return entries;
     };
 }
