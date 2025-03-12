ebfe02a76f79e9625da2b79cbc23a696c30f54c8
vitalino silva <vitalino.silva@hicx.com>
2025-03-12 10:47:20 +0000

    added workflow-editor-store

--- src/main-sections/WorkflowEditor.vue
+++ src/main-sections/WorkflowEditor.vue
@@ -6,53 +6,27 @@
 
 <script setup>
 import { ref, onMounted, onBeforeMount } from 'vue';
-import { createWorkflowEditor } from '../bpmn-workflow-editor/modeler';
-import EventBus from '../eventbus';
-import { EVENT_TYPE } from '../bpmn-workflow-editor/modeler/eventTypes';
+
+import StateManager from '../state-manager';
+
+const workflowEditorStore = StateManager.useWorkflowEditorStore();
+const { unregisterWorkflowEditorEventHandlers,
+        destroyWorkflowEditor,
+        initializeWorkflowEditor,
+        registerWorkflowEditorEventHandlers,
+      } = workflowEditorStore;
 
 const workflowEditorCanvasRef = ref(null);
-const modelerRef = ref(null);
 
 onBeforeMount(() => {
-  destroyWorkflowEditor();
   unregisterWorkflowEditorEventHandlers();
+  destroyWorkflowEditor();
 });
 
 onMounted(() => {
-  initializeWorkflowEditor();
+  initializeWorkflowEditor(workflowEditorCanvasRef.value);
   registerWorkflowEditorEventHandlers();  
 });
-
-function initializeWorkflowEditor() {
-  if(!workflowEditorCanvasRef.value) {
-    return; 
-  }
-
-  const {modeler} = createWorkflowEditor(workflowEditorCanvasRef.value);
-  modelerRef.value = modeler;
-}
-
-function destroyWorkflowEditor() {
-  if(modelerRef.value) {
-    modelerRef.value.destroy();
-  }
-}
-
-function registerWorkflowEditorEventHandlers() {
-  EventBus.on(EVENT_TYPE.UPDATE_ELEMENT, (element) => {
-    console.log('update current working element on store');
-    console.log('UPDATE_ELEMENT', element);
-  });
-
-  EventBus.on(EVENT_TYPE.UPDATE_NAVIGATION_PATH, (navigationPath) => {
-    console.log('UPDATE_NAVIGATION_PATH', navigationPath);
-  });
-}
-
-function unregisterWorkflowEditorEventHandlers() {
-  EventBus.off(EVENT_TYPE.UPDATE_ELEMENT);
-  EventBus.off(EVENT_TYPE.UPDATE_NAVIGATION_PATH);
-}
 
 </script>
 
---
+++ src/state-manager/sub-managers/workflow-editor.store.js
@@ -0,0 +1,63 @@
+import { ref } from 'vue';
+import { createWorkflowEditor } from '../../bpmn-workflow-editor/modeler';
+import EventBus from '../../eventbus';
+import { EVENT_TYPE } from '../../bpmn-workflow-editor/modeler/eventTypes';
+
+export const WorkflowEditorStoreIdentifier = 'workflow-editor-store';
+
+export function WorkflowEditorStore() {
+
+    const currentModeler = ref(null);
+    const currentWorkingElement = ref(null);
+    const currentNavigationPath = ref(null);
+
+    function initializeWorkflowEditor(canvas) {
+        if(!canvas) {
+            return;
+        }
+
+        currentModeler.value = createWorkflowEditor(canvas);
+    }
+
+    function destroyWorkflowEditor() {
+        if(currentModeler.value) {
+            currentModeler.value.modeler.destroy();
+            currentModeler.value = null;
+        }        
+    }
+
+    function registerWorkflowEditorEventHandlers() {
+
+        EventBus.on(EVENT_TYPE.UPDATE_ELEMENT, (element) => {
+            currentWorkingElement.value = element;
+        });
+    
+        EventBus.on(EVENT_TYPE.UPDATE_NAVIGATION_PATH, (navigationPath) => {
+            if(!navigationPath) {
+                currentNavigationPath.value = null;
+                return;
+            }
+
+            if(!currentNavigationPath.value || currentNavigationPath.value.length > 0) {
+                currentNavigationPath.value = null;
+            }
+
+            currentNavigationPath.value = [navigationPath];
+        });
+    }
+    
+    function unregisterWorkflowEditorEventHandlers() {
+        EventBus.off(EVENT_TYPE.UPDATE_ELEMENT);
+        EventBus.off(EVENT_TYPE.UPDATE_NAVIGATION_PATH);
+    }
+        
+    return {
+        modeler: currentModeler,
+        currentWorkingElement,
+        currentNavigationPath,
+        initializeWorkflowEditor,
+        destroyWorkflowEditor,
+        registerWorkflowEditorEventHandlers,
+        unregisterWorkflowEditorEventHandlers
+    };
+}   
