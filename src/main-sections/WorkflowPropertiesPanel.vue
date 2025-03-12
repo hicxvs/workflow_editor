097d4b162aaec3799af5a0f66f41611d48706e4b
vitalino silva <vitalino.silva@hicx.com>
2025-03-12 12:02:22 +0000

    added generic button

--- src/main-sections/WorkflowPropertiesPanel.vue
+++ src/main-sections/WorkflowPropertiesPanel.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <h1>Properties Panel</h1>
+    <Button label="Generate XML" />
   </div>
 </template>
 
 <script setup>
-
+import Button from "../components/generic/Button.vue";
 </script>
 
 <style scoped>
