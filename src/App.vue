55b7343f98855e9ba877a11b210f69dd8ef0fcd7
vitalino silva <vitalino.silva@hicx.com>
2025-03-17 17:05:41 +0000

    added singleton component loader

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="app-container">
+    <Loader />
     <FileInput />
     <Layout />
   </div>
 </template>
 
 <script setup>
+import Loader from './components/singleton-components/Loader.vue';
 import FileInput from './components/singleton-components/FileInput.vue';
 import Layout from './layout/Layout.vue';
 
