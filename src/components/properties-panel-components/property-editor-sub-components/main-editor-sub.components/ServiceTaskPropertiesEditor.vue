149b11f515a051db6ca6d0a6f915f44752458a3e
vitalino silva <vitalino.silva@hicx.com>
2025-04-02 09:15:53 +0100

    init service task

---
+++ src/components/properties-panel-components/property-editor-sub-components/main-editor-sub.components/ServiceTaskPropertiesEditor.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="service-task-properties-editor-container" data-testid="service-task-properties-editor-container">
+        here!!!
+    </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+
+import EventBus from '../../../../eventbus';
+import { EVENT_TYPE } from '../../../../bpmn-workflow-editor/modeler/eventTypes';
+
+import TextInput from '../../../generic/TextInput.vue';
+
+const model = defineModel();
+
+watch(
+  () => model, 
+  () => {
+    
+  },
+  { immediate: true, deep: true }
+);
+</script>
+
+<style scoped>
+
+</style>
