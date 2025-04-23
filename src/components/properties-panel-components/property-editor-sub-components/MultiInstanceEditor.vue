72bed48095d10961be430e6e71276caf860f8dfe
vitalino silva <vitalino.silva@hicx.com>
2025-04-23 17:29:39 +0100

    init multi instance

--- src/components/properties-panel-components/property-editor-sub-components/MultiInstanceEditor.vue
+++ src/components/properties-panel-components/property-editor-sub-components/MultiInstanceEditor.vue
@@ -12,16 +12,59 @@
 
 <script setup>
 import { ref, watch } from 'vue';
+import { ACTIVITY_TYPES } from '../../../bpmn-workflow-editor/modeler/modelerTypes/activityTypes';
 
 import Card from '../../generic/Card.vue';
+import Select from '../../generic/Select.vue';
+import TextArea from "../../generic/TextArea.vue";
 
 const model = defineModel();
+const multiInstance = ref(null);
+const isSequencial = ref(false);
+const collection = ref(null);
+const loopCardinality = ref(null);
+const completionCondition = ref(null);
+const elementVariable = ref(null);
 
 const cardProps = {
-    title: "Main Instance",
+    title: "Multi Instance",
     subtitle: "",
     text: ""
 };
+
+const multiInstanceLabels = {
+    sequencial: 'Sequencial',
+    loopCardinality: 'Loop cardinality',
+    collection: 'collection',
+    elementVariable: 'Element Variable',
+    completionCondition: 'Completion Condition'
+};
+
+const fieldKeys = {
+    sequencial: 'sequencial',
+    loopCardinality: 'loopCardinality',
+    collection: 'collection',
+    elementVariable: 'elementVariable',
+    completionCondition: 'completionCondition'
+};
+
+watch(
+  () => model, 
+  () => {
+
+    multiInstance.value = model.value?.loopCharacteristics || {};
+
+    isSequencial.value = multiInstance.value?.isSequencial || false;
+    completionCondition.value = multiInstance.value?.completionCondition?.body || '';
+    loopCardinality.value = multiInstance.value?.loopCardinality.body || '';
+    collection.value = multiInstance.value.$attrs[ACTIVITY_TYPES.COLLECTION] || '';
+    elementVariable.value = multiInstance.value.$attrs[ACTIVITY_TYPES.ELEMENT_VARIABLE] || '';
+
+    console.log( multiInstance.value);
+    
+  },
+  { immediate: true, deep: true }
+);
 
 </script>
 
