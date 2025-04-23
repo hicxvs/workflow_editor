6e23131abc434c98af1232ef42ece068eabe3362
vitalino silva <vitalino.silva@hicx.com>
2025-04-23 15:50:36 +0100

    initiated main-instance

---
+++ src/components/properties-panel-components/property-editor-sub-components/MultiInstanceEditor.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="multi-instance-panel-container" data-testid="multi-instance-panel-container">
+        <Card :title="cardProps.title" :subtitle="cardProps.subtitle" :text="cardProps.text">
+            <template #content>
+                <div v-if="model" class="main-instance-content" data-testid="main-instance-content">
+                    {{ model }}
+                </div>
+            </template>
+        </Card>
+    </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+
+import Card from '../../generic/Card.vue';
+
+const model = defineModel();
+
+const cardProps = {
+    title: "Main Instance",
+    subtitle: "",
+    text: ""
+};
+
+</script>
+
+<style scoped>
+.main-instance-content {
+    padding: 0 16px;
+}
+
+</style>
