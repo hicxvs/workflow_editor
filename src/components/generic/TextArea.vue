87134b8d22e761ff034e0b9dfd9a1e35dc9ff5d7
hicxvs <vitalino.silva@hicx.com>
2025-08-18 12:54:30 +0100

    updated

--- src/components/generic/TextArea.vue
+++ src/components/generic/TextArea.vue
@@ -33,4 +33,12 @@ defineProps({
 </script>
 
 <style scoped>
+/* start of fix attempt
+
+Attempt to fix the issue caused SXP style codebase on the workflow UI. 
+Once that code is fixed this css style can be removed*/
+.text-area-container .v-text-field .v-input__control {
+  height: initial !important;
+}
+/*end of fix attempt */
 </style>
